4e0240ac9636014bafb10ef98153135e1947275c
Donald Umeh <donumeh54@gmail.com>
2025-06-02 15:16:24 +0100

    log queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,31 @@
+import sqlite3
+import functools
+
+### decorator to log SQL queries
+
+def log_queries(func):
+    """
+    Log queries: logs all queries sent to the function
+    """
+    @functools.wraps(func)
+    
+    def wrapper(*arg, **kwargs):
+        print(f"__{func.__name__}__.{kwargs['query']}")
+        func(*arg, **kwargs)
+
+        return None
+    return wrapper
+
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+if __name__ == "__main__":
+    # users = fetch_all_users(query="CREATE TABLE users (id PRIMARY KEY, name VARCHAR)")
+    users = fetch_all_users(query="SELECT * FROM users")
