6665a8ea0ad453a59fbb329bf0c52b20469edceb
Donald Umeh <donaldumeh54@gmail.com>
2025-07-17 04:04:04 +0100

    define data models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,42 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-
+import uuid
 
 
 # Create your models here.
 class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics', blank=True, null=True)
 
+    user_id = models.CharField(default=uuid.uuid4, primary_key=True)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.PhoneNumberField()
 
-    def __str__(self):
-        return self.username 
 
 
 class Conversation(models.Model):
+    conversation_id = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     participants = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name="conversations"
     )
 
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Conversation {self.id} - Users: {", ".join(user.username for user in self.participants.all())}"
 
 
 
 class Message(models.Model):
+
+    message_id = models.CharField(max_length=255)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
     sender = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages'
     )
     conversation = models.ForeignKey(
         Conversation, on_delete=models.CASCADE, related_name='messages'
     )
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.sender.username}: {self.content[:30]}"
