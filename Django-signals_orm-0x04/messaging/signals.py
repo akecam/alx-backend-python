58bc55092a78bb4119c2cfedebb2ee596ca6a787
Donald Umeh <donaldumeh54@gmail.com>
2025-07-31 16:05:17 +0100

    use signals for deleting user-related data

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,11 +1,10 @@
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.contrib.auth import get_user_model
 
-
-
 User = get_user_model()
+
 
 @receiver(post_save, sender=Message)
 def trigger_notification(sender, instance, created, **kwargs):
@@ -17,3 +16,10 @@ def trigger_notification(sender, instance, created, **kwargs):
 def trigger_message_history(sender, instance, raw, using, update_fields, **kwargs):
     if not raw:
         MessageHistory.objects.create(message_history_content=instance)
+
+
+@receiver(post_delete, sender=User)
+def delete_user(sender, instance, using, **kwargs):
+    Notification.objects.get(user=instance).delete()
+    MessageHistory.objects.get(edited_by=instance).delete()
+    Message.objects.get(sender=instance).delete()
